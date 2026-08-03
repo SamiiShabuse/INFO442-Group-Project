12704158dc0c43798f20ae433cc92ec2dca2dbcd
Samii Shabuse <sshabuse@gmail.com>
2026-08-03 19:46:46 -0400

    generate_predictions.py: create base cmds

--- scripts/generate_predictions.py
+++ scripts/generate_predictions.py
@@ -13,3 +13,17 @@ import pandas as pd
 import numpy as np
 import joblib # helps with parallel computing
 from pandas.tseries.offsets import BDay # for business days
+
+def main(model_path, features_path, out_path):
+    model_path = Path(model_path)
+    features_path = Path(features_path)
+    out_path = Path(out_path)
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser
+    p.add_argument("--model", required=True, help="Path to trained model (joblib .pkl)")
+    p.add_argument("--features", required=True, help="Path to feature CSV (must include date, ticker info)")
+    p.add_argument("--out", required=True, help="Output CSV path (predictions)")
+    args = p.parse_args()
+    main(args.model, args.feature, args.out)

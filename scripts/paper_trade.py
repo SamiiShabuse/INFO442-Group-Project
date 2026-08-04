e960dc3a89b298c0a405762fbea323633d5dd3bf
Samii Shabuse <sshabuse@gmail.com>
2026-08-03 20:20:27 -0400

    paper_trade: create load prediction fucntion

--- scripts/paper_trade.py
+++ scripts/paper_trade.py
@@ -10,3 +10,27 @@ export ALPACA_API_KEY=...
 export ALPACA_SECRET_KEY=...
 python scripts/paper_trade.py --predictions predictions/latest_preds.csv`
 """
+import os
+import argparse
+from pathlib import Path
+import csv 
+import math
+import pandas as pd
+import numpy as np
+from datetime import datetime
+try:
+    from alpaca_tradeapi.rest import REST, APIError
+except Exception:
+    REST = None # allow dry-run without alpaca package
+
+EPS = 1e-8
+
+def load_predictions(path):
+    df = pd.read_csv(path, index_col=0, parse_dates=True)
+
+    #take last row if mutilple
+    row = df.iloc[-1]
+
+    # ensure string trickers
+    row.index = row.index.astype(str)
+    return row
